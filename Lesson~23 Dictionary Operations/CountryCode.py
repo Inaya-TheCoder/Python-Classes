3db3f9745afd3dcd0b2f9d7873a87a311b8464cb
Inaya Singh <inaya.singh2013@gmail.com>
2026-01-27 21:50:39 +0530

    Countrycode

---
+++ Lesson~23 Dictionary Operations/CountryCode.py
@@ -0,0 +1,19 @@
+SPCMcountry_code = {'India' : '0091',
+
+'Australia' : '0025',
+
+'Nepal' : '00977'}
+
+
+# search dictionary for country code of India
+
+print("Country code for India -")
+
+print(SPCMcountry_code.get('India', 'Not Found'))
+
+
+# search dictionary for country code of Japan
+
+print("Country code for USA -")
+
+print(SPCMcountry_code.get('USA', 'Not Found'))
